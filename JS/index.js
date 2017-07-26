7f07826b2c18e91d1d25e7c7670fefb315990cd1
gabrielestrela <gabrielestrela92@gmail.com>
2017-07-26 12:05:56 -0300

    Adjusted layout accordingly to client

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+    $('#privacidade').hover(function () {
+        $('#privacidade .chip').toggleClass('red-text lighten-2');
+    });
+    
+    $('#clientes').hover(function(){
+       $('#clientes .chip').toggleClass('red-text lighten-2'); 
+    });
 });
