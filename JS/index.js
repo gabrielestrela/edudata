2a82392a39901182e86a4c11d365cc9bd783d268
gabrielestrela <gabrielestrela92@gmail.com>
2017-07-31 20:22:41 -0300

    Created function to change bg in different screen sizes

--- JS/index.js
+++ JS/index.js
@@ -5,9 +5,30 @@ $(document).ready(function () {
     $('#privacidade').hover(function () {
         $('#privacidade .chip').toggleClass('red-text lighten-2');
     });
-    
-    $('#clientes').hover(function(){
-       $('#clientes .chip').toggleClass('red-text lighten-2'); 
+
+    $('#clientes').hover(function () {
+        $('#clientes .chip').toggleClass('red-text lighten-2');
     });
-    
+
+    if ($('#windowSize').css('padding') == '4px') {
+        //        console.log('entrou');
+        $('#intro').css('background-image', "url(../IMAGES/wallpapers/introTeste.jpg)");
+    }
+    if ($('#windowSize').css('padding') == '6px') {
+        $('#intro').css('background-image', "url(../IMAGES/wallpapers/introTeste.jpg)");
+    }
+
+    $(window).resize(function () {
+        if ($('#windowSize').css('padding') == '4px') {
+            //            console.log('entrou');
+            $('#intro').css('background-image', "url(../IMAGES/wallpapers/introTeste.jpg)");
+        }
+        if ($('#windowSize').css('padding') == '6px') {
+            $('#intro').css('background-image', "url(../IMAGES/wallpapers/introTeste.jpg)");
+        }
+        if($('#windowSize').css('padding') == '8px' || $('#windowSize').css('padding') == '10px'){
+            $('#intro').css('background-image', "url(../IMAGES/wallpapers/intro.jpg)");
+        }
+    });
+
 });
