e615bd9749ffbc9080de6e88b97003536e9e8fd9
gabrielestrela <gabrielestrela92@gmail.com>
2017-07-31 23:10:11 -0300

    Changed bg function from js to css

--- JS/index.js
+++ JS/index.js
@@ -10,28 +10,4 @@ $(document).ready(function () {
         $('#clientes .chip').toggleClass('red-text lighten-2');
     });
 
-    if ($('#windowSize').css('padding') == '4px') {
-        //        console.log('entrou');
-        $('#intro').css('background-image', "url(edudata/IMAGES/wallpapers/introTeste.jpg)");
-    }
-    if ($('#windowSize').css('padding') == '6px') {
-        $('#intro').css('background-image', "url(edudata/IMAGES/wallpapers/introTeste.jpg)");
-    }
-    if ($('#windowSize').css('padding') == '8px' || $('#windowSize').css('padding') == '10px') {
-        $('#intro').css('background-image', "url(../IMAGES/wallpapers/intro.jpg)");
-    }
-
-    $(window).resize(function () {
-        if ($('#windowSize').css('padding') == '4px') {
-            //            console.log('entrou');
-            $('#intro').css('background-image', "url(edudata/IMAGES/wallpapers/introTeste.jpg)");
-        }
-        if ($('#windowSize').css('padding') == '6px') {
-            $('#intro').css('background-image', "url(edudata/IMAGES/wallpapers/introTeste.jpg)");
-        }
-        if($('#windowSize').css('padding') == '8px' || $('#windowSize').css('padding') == '10px'){
-            $('#intro').css('background-image', "url(edudata/IMAGES/wallpapers/intro.jpg)");
-        }
-    });
-
 });
